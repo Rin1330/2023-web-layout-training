0bfbcc3f339395797dc6093e06a028920ce5a8d3
sakimotorin <sakimotorin@gmail.com>
2023-05-15 17:03:15 +0800

    update swiper style

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,12 +1,26 @@
+import Swiper, { Navigation, Pagination, Scrollbar } from '../../node_modules/swiper';
+
 document.addEventListener('DOMContentLoaded', () => {
   // DOM Ready!
+
+  const btn = document.querySelector('#back-to-top');
+  // Event Listener
+  btn.addEventListener('click', () => {
+    setTimeout(() => {
+      window.scrollTo({ top: 0, behavior: "smooth" })
+    }, 300);
+  })
+  // 
   const brandSwiperUpper = new Swiper('.brandWrap-swiper-upper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
     spaceBetween: 20,
-    slidesPerView: 'auto',
     freeMode: true,
+    speed: 500,
+    loop: true,
+    slidesPerView: "auto",
+    autoplay: {
+      delay: 500,
+      disableOnInteraction: false,
+      },
   })
 
   const brandSwiperLower = new Swiper('.brandWrap-swiper-lower', {
@@ -15,6 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
     loop: true,
     spaceBetween: 20,
     slidesPerView: 'auto',
+    // centeredSlides: true,
   })
 
   const commentWrap = new Swiper('.commentWrap', {
@@ -27,8 +42,20 @@ document.addEventListener('DOMContentLoaded', () => {
         slidesPerView: 3,
         spaceBetween: 24,
       },
-    }
-  })
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.commentWrap .swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+      renderBullet: (index, className) => {
+        return `<li><a href="javascript:;" class="${className}"><span>${index + 1}</span></a></li>`
+      }
 
-  // const filterWrap = new Swiper('.filterWrap')
+    }
+    
+  })
 })
